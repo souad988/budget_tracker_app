40867a54e91613f70b8aee4c44a5c3da70e78494
souad988 <souadelmansouri88@gmail.com>
2023-10-27 12:40:34 +0100

    Fix linter errors

--- spec/routing/expense_transactions_routing_spec.rb
+++ spec/routing/expense_transactions_routing_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ExpenseTransactionsController, type: :routing do
   describe 'routing' do
-    
     it 'routes to #new' do
       expect(get: '/expense_transactions/new').to route_to('expense_transactions#new')
     end
@@ -10,6 +9,5 @@ RSpec.describe ExpenseTransactionsController, type: :routing do
     it 'routes to #create' do
       expect(post: '/expense_transactions').to route_to('expense_transactions#create')
     end
-
   end
 end
